c705e0ccb178c783453f3e71f76df85a89ba7d4b
nbrown225 <chbrown225@yahoo.com>
2022-07-31 12:39:14 -0700

    added css and changed layout to look better

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ var showDate = m.format('dddd MMM Mo YYYY')
 console.log(showDate);
 console.log(moment);
 
+// --------------- task section ---------------------
 // Dynamically created div to hold buttons
 var colorBox = document.createElement('div');
 colorBox.setAttribute('class', 'colorchoice');
@@ -12,6 +13,8 @@ colorBox.setAttribute('class', 'colorchoice');
 var taskRed = document.createElement('button');
 // ---set id for css
 taskRed.setAttribute('id', 'red;');
+// ---set color of button
+taskRed.setAttribute('class', 'btn-red')
 // ---set type so page won't refresh when clicking on the button
 taskRed.setAttribute('type', 'button');
 // ---add the function for the box to change to desired color
@@ -20,14 +23,19 @@ taskRed.setAttribute('onclick', 'turnItRed()');
 
 var taskPurple = document.createElement('button');
 taskPurple.setAttribute('id', 'purple;');
+taskPurple.setAttribute('class', 'btn-purple')
 taskPurple.setAttribute('type', 'button');
 taskPurple.setAttribute('onclick', 'turnItPurple()');
+
 var taskGreen = document.createElement('button');
 taskGreen.setAttribute('id', 'green;');
+taskGreen.setAttribute('class', 'btn-green')
 taskGreen.setAttribute('type', 'button');
 taskGreen.setAttribute('onclick', 'turnItGreen()');
+
 var taskBlue = document.createElement('button');
 taskBlue.setAttribute('id', 'blue;');
+taskBlue.setAttribute('class', 'btn-blue')
 taskBlue.setAttribute('type', 'button');
 taskBlue.setAttribute('onclick', 'turnItBlue()');
 
