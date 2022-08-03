51780a06afc89cf4f992bcbfeac3e9f64e4a96df
nbrown225 <chbrown225@yahoo.com>
2022-08-02 19:38:49 -0700

    got tutor help and still unable to link hours value to timeblock.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,288 @@
+//const moment = require("moment");
+
+//const moment = require("moment");
+
 var m = moment();
 var monday = document.querySelector(".mon")
 var showCurrentDay = document.querySelector('#currentDay');
 var showDate = m.format('dddd MMM Mo YYYY')
+
 console.log(showDate);
-console.log(moment);
+//console.log(m);
+ 
+showTime = () => {
+    showCurrentDay.innerHTML = showDate
+}
+document.addEventListener('onload', showTime())
+
+
+// GET CURRENT TIME
+var currentHour = m.format('hh');
+console.log(currentHour);
+
+
+// PAST FUNCTION
+// const testA = document.getElementsById('bob')
+// console.log(testA);
+// const testb = testA.getElementsByClassName('hour-3')
+// console.log(testb);
+// PRESENT FUNCTION
+//  function presetChange () {
+//     console.log('hF');
+//     element = document.
+//     let val = element.attribute[0].value;
+//     console.log(val);
+//     //  var present = document.getElementsByTagName('tr').value
+//     // console.log(present);
+//     //  present.removeAttribute('class');
+//     //  present.setAttribute('class', 'present')
+// }
+
+//     var time10 = document.querySelector("#time-10");
+//     var getT = timeSlot.getAttribute("id").slice(5,7);
+// SET UNIQUE ID TO TIME BLOCK
+var testa = document.querySelector('test02').value
+console.log(testa);
+// LINK TIME WITH TIME BLOCK ID
+
+// SET 
+
+// SET 
+
+// WANT TIME BLOCK TO CHANGE WHEN TIME ON PRESENT TIME
+
+// 
+/*
+
+// FUTURE FUNCTION
+
+// CREATE TIMEBLOCK VARIABLES 
+//var am8 = document.querySelector('work8').value;
+//console.log(am8);
+var am9 = 09;
+var am10 = 10;
+var am11 = 11;
+var pm12 = 12;
+var pm1 = 01;
+var pm2 = 02;
+var pm3 = 03;
+var pm4 = 04;
+var pm5 = 05;
+var pm6 = 06;
+
+// IF ELSE TO CHECK IF CURRENT / PAST / FUTURE
+//--------- 8AM
+if (am8 == currentHour){
+    presetChange();
+    
+}
+else if (am8 < currentHour){
+    // pastChange()
+} 
+else if (am8 > currentHour){
+    // futureChange()
+}
+
+//---------- 9AM
+if (am9 == currentHour){
+    presetChange();
+}
+else if (am9 < currentHour){
+    // pastChange()
+} 
+else if (am9 > currentHour){
+    // futureChange()
+}
+//--------- 10AM
+if (am10 == currentHour){
+    presetChange();
+}
+else if (am10 < currentHour){
+    // pastChange()
+} 
+else if (am10 > currentHour){
+    // futureChange()
+}
+//--------- 11AM
+if (am11 == currentHour){
+    presetChange();
+}
+else if (am11 < currentHour){
+    // pastChange()
+} 
+else if (am11 > currentHour){
+    // futureChange()
+}
+//--------- 12PM
+if (pm12 == currentHour){
+    presetChange();
+}
+else if (pm12 < currentHour){
+    // pastChange()
+} 
+else if (pm12 > currentHour){
+    // futureChange()
+}
+//--------- 1PM
+if (pm1 == currentHour){
+    presetChange();
+}
+else if (pm1 < currentHour){
+    // pastChange()
+} 
+else if (pm1 > currentHour){
+    // futureChange()
+}
+//--------- 2PM
+if (pm2 == currentHour){
+    presetChange();
+}
+else if (pm2 < currentHour){
+    // pastChange()
+} 
+else if (pm2 > currentHour){
+    // futureChange()
+}
+//--------- 3PM
+if (pm3 == currentHour){
+    presetChange();
+    console.log('present');
+}
+else if (pm3 < currentHour){
+    // pastChange()
+} 
+else if (pm3 > currentHour){
+    // futureChange()
+}
+//--------- 4PM
+if (pm4 == currentHour){
+    presetChange();
+
+}
+else if (pm4 < currentHour){
+    // pastChange()
+} 
+else if (pm4 > currentHour){
+    // futureChange()
+    console.log('futru');
+}
+//--------- 5PM
+if (pm5 == currentHour){
+    presetChange();
+}
+else if (pm5 < currentHour){
+    // pastChange()
+} 
+else if (pm5 > currentHour){
+    // futureChange()
+}
+//--------- 6PM
+if (pm6 == currentHour){
+    presetChange();
+}
+else if (pm6 < currentHour){
+    // pastChange()
+} 
+else if (pm6 > currentHour){
+    // futureChange()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// TRIED USING DEFAULT ELSE INSTEAD AT THE END, BUT IT CAUSED ERRORS FOR MY RESULTS
+/*
+if (hour2 < currentHour) {
+     console.log('past');
+ } else if (hour2 == currentHour) {
+     console.log('present');
+ } else (hour2 > currentHour){
+     console.log('future');
+ }
+*/
+
+
+
+
+
+
+
+
+
+//  get time block
+//  var time12 = time12.
+//  var getT = timeSlot.getAttribute("id").slice(5,7);
+
+// var hourSlot = timeSlot.getAttribute("id")
+
+//  get id from time block
+
+//  slice to convert to number
+
+//  for loop to do the same on each slot
+
+//  this to get result? then compare to time
+
+
+
+//  had to scrap running through a for loop because i kept getting errors with adding getattribut() in the loop. 
+//  so i caved and did if else for each number
+// function timeFunc(){
+// // GET TIME
+// var m = moment();
+// // CONVERT TO HOURS
+// var currentHour = m.format('HH')
+// console.log(currentHour);
+// var timeSlot = document.querySelectorAll(".check-time");
+// for(var i = 0; i < timeSlot.length; i++) {
+//     var time10 = document.querySelector("#time-10");
+//     var getT = timeSlot.getAttribute("id").slice(5,7);
+//     console.log(getT);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // --------------- task section ---------------------
 // Dynamically created div to hold buttons
